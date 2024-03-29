725bb2ec25adf14ba852c0c91ce0bffa89a77d4f
Gabin Lee <gb94@ewhain.net>
2024-03-29 20:22:11 +0900

    Feat: week03 포트폴리오 과제 제출

---
+++ week03/app.js
@@ -0,0 +1,37 @@
+
+// 기술스택창
+const $text = document.querySelector(".mySkills");
+const skills = ["Photoshop","Figma","Procreate","HTML","CSS", "JavaScript", "C", "Python","C#","Unity", "Firebase", "React"];
+const speed = 200;
+let i = 0;
+
+const type = async () => {  
+const skill = skills[i].split("");
+
+while (skill.length) {
+    await wait(speed);
+    $text.innerHTML += skill.shift(); 
+}
+await wait(800);
+remove();
+}
+
+//remove
+const remove = async () => {
+const skill = skills[i].split("");
+
+while (skill.length) {
+    await wait(speed);
+    
+    skill.pop();
+    $text.innerHTML = skill.join(""); 
+}
+
+i = !skills[i + 1] ? 0 : i + 1;
+type();
+}
+
+function wait(ms) {
+return new Promise(res => setTimeout(res, ms))
+}
+setTimeout(type, 1500);
