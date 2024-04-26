88e5c86d006cc85fb22bb015d73df22b79f004ee
gaeun0nueag <87455786+gaeun0nueag@users.noreply.github.com>
2024-04-26 17:56:00 +0900

    todo 저장

--- week05/my-todolist/src/App.js
+++ week05/my-todolist/src/App.js
@@ -5,24 +5,17 @@ import TodoList from "./TodoList";
 import TodoAdd from "./TodoAdd";
 import { useEffect, useState } from "react";
 
+const TODO_LIST_KEY = "todoList";
+
 function App() {
-  const [todoList, setTodoList] = useState([
-    {
-      id: 1,
-      text: "EFUB 리액트 과제 제출",
-      done: true,
-    },
-    {
-      id: 2,
-      text: "시험공부",
-      done: false,
-    },
-    {
-      id: 3,
-      text: "강의 듣기",
-      done: false,
-    },
-  ]);
+  const [todoList, setTodoList] = useState(() => {
+    const saveTodoList = localStorage.getItem(TODO_LIST_KEY);
+    return saveTodoList ? JSON.parse(saveTodoList) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem(TODO_LIST_KEY, JSON.stringify(todoList));
+  }, [todoList]);
   return (
     <div className="App">
       <Header />
