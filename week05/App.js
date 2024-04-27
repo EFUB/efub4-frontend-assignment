f766d95579a2a7e1165992aeca9a98c978f3234a
sj0919 <161692586+sj0919@users.noreply.github.com>
2024-04-27 20:13:37 +0900

    [이승진]5주차 과제제출

---
+++ week05/App.js
@@ -0,0 +1,29 @@
+import Header from "./Header";
+import TodoList from "./TodoList";
+import TodoAdd from "./TodoAdd";
+import logo from './logo.svg';
+import {useEffect, useState} from "react";
+import './App.css';
+
+const TODO_LIST_KEY="todoList";
+
+function App() {
+  const [todoList,setTodoList]=useState(()=>{
+    const savedTodoList=localStorage.getItem(TODO_LIST_KEY);
+    return savedTodoList?JSON.parse(savedTodoList):[];
+  });
+
+  useEffect(()=>{
+    localStorage.setItem(TODO_LIST_KEY,JSON.stringify(todoList));
+  },[todoList]);
+  
+  return (
+   <div className="App">
+    <Header/>
+      <TodoAdd todoList={todoList} setTodoList={setTodoList}/>
+      <TodoList todoList={todoList} setTodoList={setTodoList}/>
+   </div>
+  );
+}
+
+export default App;
