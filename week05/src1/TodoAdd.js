075f6ce2c2785b45c5f0e239e7aa0b945b4a865b
ch9968 <ichanhui578@gmail.com>
2024-04-29 23:35:22 +0900

    sixth commit

---
+++ week05/src1/TodoAdd.js
@@ -0,0 +1,26 @@
+function TodoAdd({ todoList, setTodoList }) {
+  function addTodo(event) {
+    event.preventDefault();
+    const newTodoItem = {
+      id: Date.now(),
+      text: event.target["todo-text"].value,
+      done: false,
+    };
+    setTodoList([...todoList, newTodoItem]);
+    event.target.reset();
+  }
+
+  return (
+    <form className="todo-add" onSubmit={addTodo}>
+      <input
+        type="text"
+        name="todo-text"
+        required
+        placeholder="오늘의 할 일은?"
+      />
+      <input type="submit" value="+" />
+    </form>
+  );
+}
+
+export default TodoAdd;
---
+++ week05/src1/TodoItem.js
@@ -0,0 +1,25 @@
+function TodoItem({ todoThis, todoList, setTodoList }) {
+  function deleteTodo() {
+    setTodoList(todoList.filter((todoItem) => todoItem.id !== todoThis.id));
+  }
+
+  function completeTodo() {
+    setTodoList(
+      todoList.map((todoItem) => {
+        return todoItem.id === todoThis.id
+          ? { ...todoThis, done: !todoThis.done }
+          : todoItem;
+      })
+    );
+  }
+
+  return (
+    <li className="todo-item">
+      <input type="checkbox" checked={todoThis.done} onClick={completeTodo} />
+      <span>{todoThis.text}</span>
+      <input type="button" value="X" onClick={deleteTodo} />
+    </li>
+  );
+}
+
+export default TodoItem;
