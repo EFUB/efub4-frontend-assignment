61363c3bf33c2bf2280f37c5d49a7ab4d4b71cb3
Gabin Lee <gb94@ewhain.net>
2024-05-04 20:09:08 +0900

    5주차 과제 피드백 반영 및 코드 수정

---
+++ week06/second-todolist/src/Header.js
@@ -0,0 +1,8 @@
+function Header() 
+{
+    return (
+        <h1 className="header">업보를 청산하자!</h1>
+    )
+}
+
+export default Header;
---
+++ week06/second-todolist/src/TodoItem.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+
+function TodoItem({ todoThis, todoList, setTodoList }) 
+{
+    const [editingText, setEditingText] = useState(todoThis.text);
+    const [isEditing, setIsEditing] = useState(false); // 수정 모드를 관리하는 state 추가
+
+    function deleteTodo() 
+    {
+        setTodoList(todoList.filter(todoItem => todoItem.id !== todoThis.id));
+    }
+
+    function completeTodo() 
+    {
+        const currentTime = new Date().toLocaleTimeString();
+
+        setTodoList(
+            todoList.map((todoItem) => 
+            {
+                return todoItem.id === todoThis.id ? { ...todoThis, done: !todoThis.done, checkedTime: currentTime } : todoItem;
+            })
+        );
+    }
+
+    function handleTextChange(event) {
+        setEditingText(event.target.value);
+    }
+
+    function handleTextSave() {
+        setTodoList(
+            todoList.map((todoItem) => 
+            {
+                return todoItem.id === todoThis.id ? { ...todoThis, text: editingText } : todoItem;
+            })
+        );
+        setIsEditing(false); // 수정이 완료되면 수정 모드 비활성화
+    }
+
+    return (
+        <li className="todo-item">
+            <input type="checkbox" checked={todoThis.done} onChange={completeTodo} className="check-btn"/>
+            {isEditing ? (
+                <input 
+                    type="text" 
+                    value={editingText} 
+                    onChange={handleTextChange} 
+                    onBlur={handleTextSave}
+                />
+            ) : (
+                <span className={todoThis.done ? "done-text" : ""}>{todoThis.text}</span>
+            )}
+            {todoThis.checkedTime && todoThis.done && <span>{todoThis.checkedTime}</span>}
+            {todoThis.done && <span>에 내가 해냄! </span>}
+            <input type="button" value={isEditing ? "저장" : "수정"} onClick={() => setIsEditing(!isEditing)} className="edit-btn"/>
+            <input type="button" value="X" onClick={deleteTodo} className="del-btn"/>
+        </li>
+    )
+}
+
+export default TodoItem;
