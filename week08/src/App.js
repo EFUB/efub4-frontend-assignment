22eb3e5bf131b675ec3c82c7e51c8d4ca2865342
wowalswjd <alswjd5491@naver.com>
2024-05-22 20:44:33 +0900

    feat : 8주차 세미나 과제 예제 코드 업로드

---
+++ week08/src/App.js
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import DetailPage from "./pages/DetailPage";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/detail/:id" element={<DetailPage />} />
+    </Routes>
+  );
+}
+
+export default App;
